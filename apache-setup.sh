5d3da4321b85f8073561f6ef75c5aae8c82838ec
idef1x <sjoerd@sjomar.eu>
2016-09-02 11:33:47 +0200

    corrected php7 paths and corrected location in owncloud.conf

--- apache-setup.sh
+++ apache-setup.sh
@@ -1,5 +1,4 @@
 # Configure apache2 
-sed -i "s/\;default_charset/default_charset/g" /etc/php5/apache2/php.ini  
 
 # if var WEB is set then no SSL site (in case you use a SSL proxy that terminates it)
 # otherwise disable plain html
@@ -12,11 +11,12 @@ if [ -z $WEB ]
 fi 
 
 # Change max upload size and execution time
-sed -i "s/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 1024M/g" /etc/php5/apache2/php.ini
-sed -i "s/post_max_size\ \=\ 8M/post_max_size\ \=\ 1048M/g" /etc/php5/apache2/php.ini
-sed -i "s/max_execution_time\ \=\ 30/max_execution_time\ \=\ 300/g" /etc/php5/apache2/php.ini
+sed -i "s/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 1024M/g" /etc/php/7.0/apache2/php.ini
+sed -i "s/post_max_size\ \=\ 8M/post_max_size\ \=\ 1048M/g" /etc/php/7.0/apache2/php.ini
+sed -i "s/max_execution_time\ \=\ 30/max_execution_time\ \=\ 300/g" /etc/php/7.0/apache2/php.ini
 
 # Moving owncloud to root of webserver
 rm -rf /var/www/html
 mv /var/www/owncloud /var/www/html
+sed -i "s/www\/owncloud/www\/html/g" /etc/apache2/conf-available/owncloud.conf
 
